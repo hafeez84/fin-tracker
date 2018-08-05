ea9fb3c29188ee63aab3427e0f900c9c1c7fe4ee
hafeez84 <h.qasemy@live.com>
2018-08-05 14:54:20 +0300

    added ajax and update view and errors

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,17 +1,11 @@
 class StocksController < ApplicationController
 	def search
-		if params[:stock].present?
+		if params[:stock].blank?
+			flash.now[:danger] = "You have entered an empty string !"	
+		else		
 			@st = Stock.new_from_lookup(params[:stock])
-			if @st
-				render 'users/my_portfolio'
-			else
-				flash[:danger] = "You have entred and inccorect symbol !"
-				redirect_to my_portfolio_path
-			end
-		else
-			flash[:danger] = "You have entered an empty string !"
-			redirect_to my_portfolio_path
+			flash.now[:danger] = "You have entred and inccorect symbol !" unless @st
 		end
+		render partial: 'users/results'	
 	end
-	
 end
